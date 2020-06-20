a44d6f3dfa12b7721b853c894406dd6ae922b060
unknown <johnphtdo@gmail.com>
2020-06-20 00:47:03 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/UTA_Lambda_Phi_Epsilon/precache-manifest.4abe537849006b719422108d844cd1ce.js"
+  "/UTA_Lambda_Phi_Epsilon/precache-manifest.10fc185a5690b04d65896a8730a824a7.js"
 );
 
 self.addEventListener('message', (event) => {
