aa896a57a2f8b2126acf250da2b3628f0c9e19f5
Dohn Joe <johnphtdo@gmail.com>
2020-07-07 10:52:50 -0500

    installed mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+
+// Setting up Mongoose Connection
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/" + PORT);
 
 // Send every other request to the React app
 // Define any API routes before this runs
